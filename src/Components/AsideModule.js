5553ac92c5039ca4886180c3c686ac5c9adeadb6
Wojciech Fydrych <wfydrych@gmail.com>
2020-03-09 19:02:38 +0100

    React Router added

--- src/Components/AsideModule.js
+++ src/Components/AsideModule.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './AsideModule.sass';
+import {NavLink, BrowserRouter} from 'react-router-dom'
 
 
 const AsideModule = () => (
     <nav>
         <ul>
-            <li><i className="icon-user"/> O MNIE</li>
-            <li><i className="icon-cog-alt"/> TECHNOLOGIE</li>
-            <li><i className="icon-picture"/> PROJEKTY</li>
-            <li><i className="icon-mail-alt"/> KONTAKT</li>
+            <BrowserRouter>
+            <NavLink to='/about'><li><i className="icon-user"/> O MNIE</li></NavLink>
+            <NavLink to='/technologies'><li><i className="icon-cog-alt"/> TECHNOLOGIE</li></NavLink>
+            <NavLink to='/projects'><li><i className="icon-picture"/> PROJEKTY</li></NavLink>
+            <NavLink to='/contact'><li><i className="icon-mail-alt"/> KONTAKT</li></NavLink>
+            </BrowserRouter>
         </ul>
     </nav>
 )
--- src/Components/ContentModule.js
+++ src/Components/ContentModule.js
@@ -1,15 +1,62 @@
 import React from 'react';
 import './ContentModule.sass';
+import {Route, Switch, BrowserRouter} from 'react-router-dom'
+
+const about = () => { 
+    return (
+    <React.Fragment>
+    <h3>Welcome on my page!</h3>
+    <span>
+        I am a junior web-developer, starting my journey with IT.
+        On my page you will find projects which I've already done, and also technologies which I know so far.
+        In case you would see me in my team, feel free to contact.
+    </span>
+    </React.Fragment>
+)}
+
+const technologies = () => { 
+    return (
+    <React.Fragment>
+    <h3>TECHNOLOGIES</h3>
+    <span>
+        HTML5, CSS3, Sass, JavaScript, jQuery<br></br>
+        React, React Router, Node.js, Express<br></br>
+        Git, Figma<br></br>
+    </span>
+    </React.Fragment>
+)}
+
+const projects = () => { 
+    return (
+    <React.Fragment>
+    <h3>PROJECTS</h3>
+    <span>
+        Portfolio, strona o bananie
+    </span>
+    </React.Fragment>
+)}
+
+const contact = () => { 
+    return (
+    <React.Fragment>
+    <h3>CONTACT</h3>
+    <span>
+        LinkedIn, telefon szwajcarski
+    </span>
+    </React.Fragment>
+)}
 
 const ContentModule = () => (
-    <div class="mainContent">
-        <h3>Welcome on my page!</h3>
-        <span>
-            I am a junior web-developer, starting my journey with IT.
-            On my page you will find projects which I've already done, and also technologies which I know so far.
-            In case you would see me in my team, feel free to contact.
-        </span>
-    </div>
+    <section class="mainContent">
+    <BrowserRouter>
+    <Switch>
+        <Route path='/about' component={about}></Route>
+        <Route path='/technologies' component={technologies}></Route>
+        <Route path='/projects' component={projects}></Route>
+        <Route path='/contact' component={contact}></Route>
+    </Switch>
+    </BrowserRouter>
+    </section>
 )
 
 export default ContentModule
