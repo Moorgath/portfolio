a9d960da617315ccdf39ba9ec9de1af02ac105bf
Wojciech Fydrych <wfydrych@gmail.com>
2020-03-11 19:20:51 +0100

    Added basic mobile version

--- src/Components/AsideModule.js
+++ src/Components/AsideModule.js
@@ -4,7 +4,8 @@ import {NavLink, withRouter} from 'react-router-dom'
 
 const AsideModule = (props) => 
     (
-    <nav>
+    <React.Fragment>
+    <nav className="navComp">
         <ul>
             <NavLink to='/about'><li><i className="icon-user"/> {props.txt.about}</li></NavLink>
             <NavLink to='/technologies'><li><i className="icon-cog-alt"/> {props.txt.tech}</li></NavLink>
@@ -12,6 +13,13 @@ const AsideModule = (props) =>
             <NavLink to='/contact'><li><i className="icon-mail-alt"/> {props.txt.cont}</li></NavLink>
         </ul>
     </nav>
+    <nav className="navMobile">
+            <NavLink to='/about'><i className="icon-user"/></NavLink>
+            <NavLink to='/technologies'><i className="icon-cog-alt"/></NavLink>
+            <NavLink to='/projects'><i className="icon-picture"/></NavLink>
+            <NavLink to='/contact'><i className="icon-mail-alt"/></NavLink>
+    </nav>
+    </React.Fragment>
 )
 
 export default withRouter(AsideModule)
